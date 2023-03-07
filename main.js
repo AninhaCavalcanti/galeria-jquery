8e02c68dd84c039be6d5298653ae7dcd97e98eb8
Ana Maria <aninha.cavalcanti.0123@gmail.com>
2023-03-07 20:52:36 -0300

    add

--- main.js
+++ main.js
@@ -12,7 +12,6 @@ $(document).ready(function() {
     const enderecoDaNovaImagem = $('#endereco-imagem-nova').val();
     const novoItem = $('<li style="display: none"></li>');
     $(`<img src="${enderecoDaNovaImagem}"/>`).appendTo(novoItem);
-    $(`<img src="${enderecoDaNovaImagem}"/>`).appendTo(novoItem);
     $(`
       <div class="overlay-img-link">
         <a href="${enderecoDaNovaImagem}" target="_blank" title="Ver imagem em tamanho real">
